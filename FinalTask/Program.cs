689583a684977129d53fef9ae7da3a484448bde4
OvcS <gerkules007@yahoo.com>
2021-12-28 21:33:33 +0300

    Add ReverseFillEvenNewArray()

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -9,5 +9,13 @@ int FindEvenNumbers(int[] inputArr)
     return inputArr.Where(e => e % 2 == 0).Count();
 }
 
-// int[] a = { 1, 2, 3, 4, 5 };
-// System.Console.WriteLine(FindEvenNumbers(a));
+void ReverseFillEvenNewArray(int[] inputArr, int[] changedArr)
+{
+    for (int i = inputArr.Length - 1, leng = changedArr.Length - 1; i >= 0; i--) 
+      if (inputArr[i] % 2 == 0) changedArr[leng--] = inputArr[i];
+}
+
+int[] currArr = { 6, 7, 8, 9, -14 };
+int[] newArr = new int [FindEvenNumbers(currArr)];
+ReverseFillEvenNewArray(currArr, newArr);
+PrintArray(newArr);
