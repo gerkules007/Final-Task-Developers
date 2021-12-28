b6965bd9fa99c5e4316a0949f4ff469df82c4be8
OvcS <gerkules007@yahoo.com>
2021-12-28 22:12:01 +0300

    Add TEST programm

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -15,7 +15,20 @@ void ReverseFillEvenNewArray(int[] inputArr, int[] changedArr)
       if (inputArr[i] % 2 == 0) changedArr[leng--] = inputArr[i];
 }
 
-int[] currArr = { 0, 7, 8, 4, -14 };
+// TEST
+// Random Test
+// void RandomArray(int[] inputArr)
+// {
+//     for (int i = 0; i < inputArr.Length; i++) inputArr[i] = new Random().Next(-10,11);
+// }
+// int[] currArr = new int [new Random().Next(15)];
+// RandomArray(currArr);
+// PrintArray(currArr);
+
+int[] currArr = { 0, 7, 1, -14, 4 }; // Массив с 0 и отрицательным числом
+// int[] currArr = { 1, 7, 1, 9 }; // Все нечетные
+// int[] currArr = new int [0]; // Пустой массив
+
 int[] newArr = new int [FindEvenNumbers(currArr)];
 ReverseFillEvenNewArray(currArr, newArr);
 PrintArray(newArr);
