846bef6dcbad0b4da0d6232b6b8bac7500f53c57
OvcS <gerkules007@yahoo.com>
2021-12-28 21:03:14 +0300

    Add FindEvenNumbers()

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -3,3 +3,11 @@
     foreach (int n in inputArr) Console.Write(n + " ");
     Console.WriteLine();
 }
+
+int FindEvenNumbers(int[] inputArr)
+{
+    return inputArr.Where(e => e % 2 == 0).Count();
+}
+
+// int[] a = { 1, 2, 3, 4, 5 };
+// System.Console.WriteLine(FindEvenNumbers(a));
